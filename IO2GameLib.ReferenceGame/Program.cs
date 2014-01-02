8014f66191eac6c76034b81f3f525a6899cf29e5
johan.kson@gmail.com <kaninbur99%>
2014-01-02 09:27:00 +0100

    Added content project and made it build

---
+++ IO2GameLib.ReferenceGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IO2GameLib.ReferenceGame
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<TheReferenceGame>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+        }
+    }
+}
---
+++ IO2GameLib.ReferenceGame/TheReferenceGame.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace IO2GameLib.ReferenceGame
+{
+    /// <summary>
+    /// This is a sample for usage of io2Gamelib for Windows 8
+    /// </summary>
+    public class TheReferenceGame : io2GameLib.Io2GameLibGame
+    {
+        public TheReferenceGame()
+        {
+        }
+    }
+}
